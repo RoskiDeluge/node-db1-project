009f31643da76b3319cb3061b8169bcebf6c5dce
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2020-04-08 17:08:21 -1000

    POST working

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -34,6 +34,18 @@ router.get('/:id', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+  const accountData = req.body;
+
+  db('accounts')
+  .insert(accountData)
+  .then(account => {
+    res.status(201).json(account)
+  })
+  .catch(err => {
+    res.status(500).json({ message: "Error posting account", err })
+  })
+});
 
 
 
